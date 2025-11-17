a70cd61c3c87fb4ce6c432665e99df8951e48ae2
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-11-18 00:11:42 +0100

    fix(init): use re.fullmatch instead of re.match
    also add anchors to LEGAL_TITLE_REGEX

--- mwutil/constants.py
+++ mwutil/constants.py
@@ -6,4 +6,4 @@ SUPPORTED_BRANCHES = [
 ]
 
 # https://www.mediawiki.org/wiki/Manual:$wgLegalTitleChars
-LEGAL_TITLE_REGEX = r"[ %!\"$&\'()*,\-.\/0-9:;=?@A-Z\\^_`a-z~\x80-\xFF+]+"
+LEGAL_TITLE_REGEX = r"^[ %!\"$&\'()*,\-.\/0-9:;=?@A-Z\\^_`a-z~\x80-\xFF+]+$"
