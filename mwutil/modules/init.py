0efccdd49c5f1f9798ee2f08fef70730e624a256
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-11-18 00:12:30 +0100

    fix(init): handle allow_empty correctly for auto values

--- mwutil/modules/init.py
+++ mwutil/modules/init.py
@@ -165,8 +165,8 @@ class Init(GlobalMWUtilModule):
                     if option.validation_pattern:
                         if not re.fullmatch(option.validation_pattern, value):
                             print_warning(console, f"Auto-configured value for {option.key} does not match validation pattern!")
-                        if not option.allow_empty and value == "":
-                            print_warning(console, f"Auto-configured value for {option.key} is empty but empty values are not allowed!")
+                    if not option.allow_empty and value == "":
+                        print_warning(console, f"Auto-configured value for {option.key} is empty but empty values are not allowed!")
                     options[option.key] = value
                 else:
                     options[option.key] = default
